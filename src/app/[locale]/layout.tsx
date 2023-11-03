a9359187a68e0554e2bdb4edac61f165434844fb
kkorabiowski <konradkorabiowski@gmail.com>
2023-11-03 14:27:47 +0100

    fix not found page

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,30 +1,10 @@
 import { Raleway as FontSans } from 'next/font/google';
 import { notFound } from 'next/navigation';
-import { NextIntlClientProvider } from 'next-intl';
+import { createTranslator, NextIntlClientProvider } from 'next-intl';
 
 import { ContactSection } from '@/components/layout/contact/contact-section';
 import { Footer } from '@/components/layout/footer/footer';
 import { Navigation } from '@/components/layout/navigation/navigation';
-
-export const fontSans = FontSans({
-  subsets: ['latin-ext'],
-  variable: '--font-sans',
-});
-const locales = ['pl', 'de'];
-
-export function generateStaticParams() {
-  return [{ locale: 'pl' }, { locale: 'de' }];
-}
-
-export async function generateMetadata({
-  params: { locale },
-}: {
-  params: { locale: string };
-}) {
-  return {
-    title: 'KJ WORK GROUP',
-  };
-}
 
 type Props = {
   children: React.ReactNode | React.ReactNode[];
@@ -33,19 +13,34 @@ type Props = {
   };
 };
 
+async function getMessages(locale: string) {
+  try {
+    return (await import(`../../locales/${locale}.json`)).default;
+  } catch (error) {
+    notFound();
+  }
+}
+
+export const fontSans = FontSans({
+  subsets: ['latin-ext'],
+  variable: '--font-sans',
+});
+
+export async function generateMetadata({ params: { locale } }: Props) {
+  const messages = await getMessages(locale);
+  const t = createTranslator({ locale, messages });
+
+  return {
+    title: 'KJ WORK GROUP',
+  };
+}
+
 export default async function LocaleLayout({
   children,
   params: { locale },
 }: Props) {
-  const isValidLocale = locales.some(cur => cur === locale);
-  if (!isValidLocale) notFound();
+  const messages = await getMessages(locale);
 
-  let messages;
-  try {
-    messages = (await import(`../../locales/${locale}.json`)).default;
-  } catch (error) {
-    notFound();
-  }
   return (
     <html lang={locale} className={fontSans.variable}>
       <body>
--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -1,5 +1,26 @@
 'use client';
 
-const NotFoundPage = () => <h1>Not found!</h1>;
+import Link from 'next/link';
+import { useTranslations } from 'next-intl';
 
-export default NotFoundPage;
+import { Container } from '@/components/commons/container';
+
+export default function NotFound() {
+  const t = useTranslations('error');
+  return (
+    <Container className="h-full min-h-full">
+      <div className="flex h-full min-h-full gap-10 justify-center items-center">
+        <h1 className="text-9xl text-primary">404</h1>
+        <div>
+          <h2>{t('message')}</h2>
+          <p>
+            {t('back')}
+            <Link href="/" className="text-secondary ml-2 font-bold">
+              {t('home')}
+            </Link>
+          </p>
+        </div>
+      </div>
+    </Container>
+  );
+}
